c4413d029cd75f77925c37a92c073fe4535093c2
Ping <pinghe2022@u.northwestern.edu>
2022-11-15 17:58:05 -0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // baseUrl: 'http://localhost:3000',
-    baseUrl: 'https://split-abdf6.web.app/',
+    baseUrl: 'http://localhost:3000',
+    // baseUrl: 'https://split-abdf6.web.app/',
   },
 });
