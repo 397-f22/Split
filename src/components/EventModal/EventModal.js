6f3e6db9b68cab71b411f6378cc9da17f419adb2
alfenn <52635707+alfenn@users.noreply.github.com>
2022-11-20 16:57:27 -0600

    add calendar ui, get it updating db

--- src/components/EventModal/EventModal.js
+++ src/components/EventModal/EventModal.js
@@ -1,7 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Badge, Button, Modal } from "react-bootstrap";
 import { useDbUpdate } from "../../utilities/firebase";
 import { Form, InputGroup } from "react-bootstrap";
+
+
+export const useFormData = (values = {}) => {
+  const [state, setState] = useState(() => ({ values }));
+  const change = (evt) => {
+    const { id, value } = evt.target;
+    if (id === "deadline") {
+      // parse the datetime value
+      var date = new Date(value);
+      date = new Intl.DateTimeFormat("en-US", {
+        dateStyle: "short",
+        timeStyle: "short",
+      }).format(date);
+      const newValues = { ...state.values, [id]: date.replace(",", " @") };
+      setState({ values: newValues });
+    } else {
+      const values = { ...state.values, [id]: value };
+      setState({ values });
+    }
+  };
+  return [state, change];
+};
+
+const InputDatetimeField = ({ name, state, change }) => {
+  console.log('this is the deadline stored in state')
+  console.log(state.values?.[name]);
+  return(
+  <div>
+    <label htmlFor={name} className="form-label">
+      Pick time
+    </label>
+    <input
+      type="datetime-local"
+      className="form-control mb-3"
+      id={name}
+      name={name}
+      defaultValue={''}
+      onChange={change}      
+    ></input>
+  </div>
+  // <DatePicker
+  //     selected={state.values.datetime}
+  //     onChange={(date) => {
+  //         change({ target: { id: name, value: date } })
+  //     }}
+  //     showTimeSelect
+  //     dateFormat="Pp"
+  // />
+);}
+
 
 const EventModal = ({
   show,
@@ -13,6 +63,9 @@ const EventModal = ({
 
 }) => {
   const [updateData] = useDbUpdate("/");
+  console.log(event.deadline);
+  const [state, change] = useFormData({ deadline: event.deadline });
+  console.log(state);
 
   const attendeesIds = Object.values(event.attendees);
   const attendees = Object.entries(users).filter(([id, user]) =>
@@ -41,16 +94,11 @@ const EventModal = ({
             Organizer: {users[event.organizer].displayName}
           </p>
           {event.organizer === currentUser.uid ?
-                  <InputGroup className="mb-3">
-                    <InputGroup.Text id="basic-addon1"> Deadline </InputGroup.Text>
-                    <Form.Control
-                      aria-label="Deadline"
-                      onChange={(e) => {
-                        event.deadline = e.target.value;
-                        updateData({ ["/events/" + eventId + "/payments"]: event.deadline });
-                      }}
-                    />
-                  </InputGroup>
+                  <InputDatetimeField name='deadline' text='Deadline' state={state} change={(e) => {
+                    change(e);
+                    console.log(state);
+                    updateData({ ["/events/" + eventId + "/deadline"]: state.values.deadline });
+                  }} />                                                                          
                   : ""}
         </div>
         <div className="modal-attendees">
