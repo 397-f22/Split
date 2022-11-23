72b16bbd0c97e9cbcc4c51332379043dc5878392
ShaliniR8 <1705820@kiit.ac.in>
2022-11-22 19:05:35 -0600

    adding description for new event to database

--- src/components/AddEventFormModal/AddEventFormModal.js
+++ src/components/AddEventFormModal/AddEventFormModal.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 import { Modal, Button, InputGroup, FloatingLabel } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import { Badge } from "react-bootstrap";
-import { useDbUpdate, updateData } from "../../utilities/firebase";
+import { useDbUpdate } from "../../utilities/firebase";
+import { v4 as uuidv4 } from "uuid";
+
 
 export const useFormData = (values = {}) => {
   const [state, setState] = useState(() => ({ values }));
@@ -26,8 +28,6 @@ export const useFormData = (values = {}) => {
 };
 
 const InputDatetimeField = ({ name, state, change }) => {
-  console.log("this is the deadline stored in state");
-  console.log(state.values?.[name]);
   return (
     <div>
       <label htmlFor={name} className="form-label">
@@ -42,52 +42,60 @@ const InputDatetimeField = ({ name, state, change }) => {
         onChange={change}
       ></input>
     </div>
-    // <DatePicker
-    //     selected={state.values.datetime}
-    //     onChange={(date) => {
-    //         change({ target: { id: name, value: date } })
-    //     }}
-    //     showTimeSelect
-    //     dateFormat="Pp"
-    // />
-  );
-};
-
-const AddEventForm = ({}) => {
-  const [state, change] = useFormData();
-  return (
-    <div>
-      <InputDatetimeField
-        name="deadline"
-        text="Deadline"
-        state={state}
-        change={(e) => {
-          change(e);
-          // console.log(state);
-          // updateData({ ["/events/" + eventId + "/deadline"]: state.values.deadline });
-        }}
-      />
-      <FloatingLabel controlId="floatingTextarea2" label="Description">
-        <Form.Control
-          as="textarea"
-          placeholder="Description"
-          style={{ height: "100px" }}
-        />
-      </FloatingLabel>
-    </div>
   );
 };
 
 const AddEventFormModal = ({ show, handleClose, currentUser }) => {
+  const [state, change] = useFormData({deadline: new Date()});
+  const [description, setDescription] = useState("")
+  const [updateData] = useDbUpdate("/");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (description.length > 0) {
+      const data = {
+        attendees: {},
+        deadline: "",
+        description: description,
+        organizer: currentUser.uid,
+        payments: {},
+        title: "title"
+      };
+      updateData({ ["events/" + uuidv4()  ]: data });
+    }
+    handleClose();
+  };
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Create A New Event</Modal.Title>
       </Modal.Header>
       <Modal.Body>        
-        <AddEventForm />        
+      <div>
+      <InputDatetimeField
+        name="deadline"
+        text="Deadline"
+        state={state}
+        // change={(e) => {
+        //   change(e);
+        //   // console.log(state);
+        //   updateData({ ["events/" +  + "/deadline"]: state.values.deadline });
+        // }}
+      />
+      <FloatingLabel controlId="floatingTextarea2" label="Description">
+        <Form.Control
+          as="textarea"
+          placeholder="Description"
+          style={{ height: "100px" }}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </FloatingLabel>
+      
+    </div>       
       </Modal.Body>
       <Modal.Footer>
+      <Button variant="primary" onClick={(e) => handleSubmit(e)}>
+          Save Changes
+        </Button>
         <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
