4fc0bd33ac4c403cf433185187c5ba606837e8ad
Camilo <camilochafloque@gmail.com>
2022-12-01 18:29:29 -0600

    Changed firebase deploy file

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -5,33 +5,38 @@ import { signInWithGoogle } from "../utilities/firebase";
 import { useProfile } from "../utilities/userProfile";
 
 const Login = () => {
-    const [user, error, isLoading] = useProfile();
-    const location = useLocation();
+  const [user, error, isLoading] = useProfile();
+  const location = useLocation();
 
-    if (error) return <h1>Error loading user: {`${error}`}</h1>;
-    if (isLoading) return <h1>Loading user profile</h1>;
+  if (error) return <h1>Error loading user: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading user profile</h1>;
 
-    return (
-        <Container className="login-body" fluid="true">
-            {user && <Navigate to={"/" + (location.state ? location.state.inviteLink : "")} />}
-            <ListGroup className="login-container m-2">
-                <ListGroup.Item className="login-card px-5 py-4">
-                    <div className="login-title-div mt-4 mb-5 d-flex">
-                        <Image className="login-image" src="favicon.png" />
-                        <h2 className="ms-2">Split</h2>
-                    </div>
-                    <Row className="login-description-div">
-                        <p className="mb-5">
-                            Welcome to Split - an application that helps you manage payments on your night out!
-                        </p>
-                        <Button className="login-button" onClick={signInWithGoogle}>
-                            SIGN IN NOW
-                        </Button>
-                    </Row>
-                </ListGroup.Item>
-            </ListGroup>
-        </Container>
-    );
+  return (
+    <Container className="login-body" fluid="true">
+      {user && (
+        <Navigate
+          to={"/" + (location.state ? location.state.inviteLink : "")}
+        />
+      )}
+      <ListGroup className="login-container m-2">
+        <ListGroup.Item className="login-card px-5 py-4">
+          <div className="login-title-div mt-4 mb-5 d-flex">
+            <Image className="login-image" src="favicon.png" />
+            <h2 className="ms-2">Split</h2>
+          </div>
+          <Row className="login-description-div">
+            <p className="mb-5">
+              Welcome to Split - an application that helps you manage payments
+              on your night out!
+            </p>
+            <Button className="login-button" onClick={signInWithGoogle}>
+              SIGN IN NOW
+            </Button>
+          </Row>
+        </ListGroup.Item>
+      </ListGroup>
+    </Container>
+  );
 };
 
 export default Login;
