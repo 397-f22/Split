16472fb05388f4f887ab5327fa8e959885ae21bd
derrickrosefan <114096803+derrickrosefan@users.noreply.github.com>
2022-12-06 13:15:21 -0600

    move mocks to helper file

--- src/components/AddEventFormModal/AddEventFormModal.test.jsx
+++ src/components/AddEventFormModal/AddEventFormModal.test.jsx
@@ -1,93 +1,10 @@
 import { describe, it, vi } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../../App';
-import { useDbData, useAuthState, useDbUpdate } from '../../utilities/firebase';
-
-const mockData = {
-  events: {
-    'testEvent': {
-      "attendees": [
-        "testUser1",
-        "testUser2",
-      ],
-      "deadline": "2022-12-01T04:13:23",
-      "description": "This is a test event",
-      "organizer": "testUser1",
-      "payments": [
-        {
-          "amount": 0,
-          "isPaid": true,
-          "paid": false,
-          "user": "testUser1",
-        }
-      ],
-      "title": "Test Event",
-    },
-  },
-  users: {
-    'testUser1': {
-      "displayName": "Test User 1",
-      "email": "testuser1@gmail.com",
-      "events": [
-        "testEvent",
-      ]
-    },
-    'testUser2': {
-      "displayName": "Test User 2",
-      "email": "testuser2@gmail.com",
-      "events": [
-        "testEvent",
-      ]
-    },
-  },
-};
+import { mockData, mockFirebase } from '../../helpers';
 
 // mock firebase
 vi.mock('../../utilities/firebase')
-const mockFirebase = (mockUser) => {
-  // mock auth
-  useAuthState.mockReturnValue([mockUser, null]);
-  // mock db
-  useDbData.mockImplementation((path) => {
-    if (path === '/events') {
-      return [mockData.events, null];
-    } else if (path === '/users') {
-      return [mockData.users, null];
-    }
-  });
-  // mock profile
-  // mock db update, update the mockData with new data
-  // data format: {path: data}
-  useDbUpdate.mockImplementation((path) => {
-    const updateData = (data) => {
-      Object.entries(data).forEach(([path, data]) => {
-        const eventsRegexMatches = path.match(/events\/([^\/]+)$/);
-        const paymentsRegexMatches = path.match(/events\/([^\/]+)\/payments$/);
-        if (eventsRegexMatches !== null) {
-          if (data['attendees']) {
-            data['attendees'] = Object.values(data['attendees']);
-          }
-          if (data['payments']) {
-            data['payments'] = Object.values(data['payments']);
-          }
-          const eventId = eventsRegexMatches[1];
-          // if new event - use a deterministic uuid?
-          if (mockData.events[eventId] === undefined) {
-            mockData.events.testAddEvent = data;
-          } else {
-            mockData.events[eventId] = data;
-          }
-        }
-        if (paymentsRegexMatches !== null) {
-          const eventId = paymentsRegexMatches[1];
-          mockData.events[eventId].payments = data;
-        }
-      });
-    }
-    return [updateData, null];
-  });
-
-};
 /**
  * @jest-environment jsdom
  */
---
+++ src/helpers.jsx
@@ -0,0 +1,90 @@
+import { useDbData, useAuthState, useDbUpdate } from './utilities/firebase';
+
+export const mockData = {
+    events: {
+        'testEvent': {
+            "attendees": [
+                "testUser1",
+                "testUser2",
+            ],
+            "deadline": "2022-12-01T04:13:23",
+            "description": "This is a test event",
+            "organizer": "testUser1",
+            "payments": [
+                {
+                    "amount": 0,
+                    "isPaid": true,
+                    "paid": false,
+                    "user": "testUser1",
+                },
+                {
+                    "amount": 30,
+                    "isPaid": true,
+                    "paid": false,
+                    "user": "testUser2",
+                }
+            ],
+            "title": "Test Event",
+        },
+    },
+    users: {
+        'testUser1': {
+            "displayName": "Test User 1",
+            "email": "testuser1@gmail.com",
+            "events": [
+                "testEvent",
+            ]
+        },
+        'testUser2': {
+            "displayName": "Test User 2",
+            "email": "testuser2@gmail.com",
+            "events": [
+                "testEvent",
+            ]
+        },
+    },
+};
+export const mockFirebase = (mockUser) => {
+    // mock auth
+    useAuthState.mockReturnValue([mockUser, null]);
+    // mock db
+    useDbData.mockImplementation((path) => {
+        if (path === '/events') {
+            return [mockData.events, null];
+        } else if (path === '/users') {
+            return [mockData.users, null];
+        }
+    });
+    // mock profile
+    // mock db update, update the mockData with new data
+    // data format: {path: data}
+    useDbUpdate.mockImplementation((path) => {
+        const updateData = (data) => {
+            Object.entries(data).forEach(([path, data]) => {
+                const eventsRegexMatches = path.match(/events\/([^\/]+)$/);
+                const paymentsRegexMatches = path.match(/events\/([^\/]+)\/payments$/);
+                if (eventsRegexMatches !== null) {
+                    if (data['attendees']) {
+                        data['attendees'] = Object.values(data['attendees']);
+                    }
+                    if (data['payments']) {
+                        data['payments'] = Object.values(data['payments']);
+                    }
+                    const eventId = eventsRegexMatches[1];
+                    // if new event - use a deterministic uuid?
+                    if (mockData.events[eventId] === undefined) {
+                        mockData.events.testAddEvent = data;
+                    } else {
+                        mockData.events[eventId] = data;
+                    }
+                }
+                if (paymentsRegexMatches !== null) {
+                    const eventId = paymentsRegexMatches[1];
+                    mockData.events[eventId].payments = data;
+                }
+            });
+        }
+        return [updateData, null];
+    });
+
+};
